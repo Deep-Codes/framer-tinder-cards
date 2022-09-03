126637031dd54ab599f4101152c217c9e60b72c1
Deep-Codes <statisticsaffinity@gmail.com>
2022-09-03 10:28:48 +0530

    feat: mvp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { AnimatePresence, motion, PanInfo } from "framer-motion";
 import type { NextPage } from "next";
 
 const CARDS = [
@@ -11,24 +13,128 @@ const CARDS = [
   { id: 7, emoji: "🍇", name: "Grapes", color: "#BC2A6E" },
 ];
 
+type ArrayElementType<ArrType> = ArrType extends readonly (infer ElementType)[]
+  ? ElementType
+  : never;
+
+type CardType = ArrayElementType<typeof CARDS>;
+
+type SwipeType = "like" | "nope";
+
+type ResultType = { [k in SwipeType]: number };
+
+type HistoryType = CardType & { swipe: SwipeType };
+
+interface CardProps {
+  card: CardType;
+  active: boolean;
+  removeCard: (oldCard: CardType, swipe: SwipeType) => void;
+}
+
 const Home: NextPage = () => {
+  const [cards, setCards] = useState(CARDS);
+  const [result, setResult] = useState<ResultType>({ like: 0, nope: 0 });
+  const [history, setHistory] = useState<HistoryType[]>([]);
+  // index of last card
+  const activeIndex = cards.length - 1;
+  const removeCard = (oldCard: CardType, swipe: SwipeType) => {
+    setHistory((current) => [...current, { ...oldCard, swipe }]);
+    setCards((current) =>
+      current.filter((card) => {
+        return card.id !== oldCard.id;
+      })
+    );
+    setResult((current) => ({ ...current, [swipe]: current[swipe] + 1 }));
+  };
+  const undoSwipe = () => {
+    const newCard = history.pop();
+    if (newCard) {
+      const { swipe } = newCard;
+      setHistory((current) =>
+        current.filter((card) => {
+          return card.id !== newCard.id;
+        })
+      );
+      setResult((current) => ({ ...current, [swipe]: current[swipe] - 1 }));
+      setCards((current) => [...current, newCard]);
+    }
+  };
   return (
     <div className="relative flex flex-col bg-black justify-center items-center w-full h-screen">
-      {CARDS.map((card) => (
+      <AnimatePresence>
+        {cards.map((card, index) => (
+          <Card
+            key={card.name}
+            active={index === activeIndex}
+            removeCard={removeCard}
+            card={card}
+          />
+        ))}
+      </AnimatePresence>
+      <footer className="absolute bottom-10">
+        <button onClick={undoSwipe}>Undo</button>
+        <span> ❤️ {result.like}</span>
+        <span> ❌ {result.nope}</span>
+      </footer>
+    </div>
+  );
+};
+
+const Card: React.FC<CardProps> = ({ card, removeCard, active }) => {
+  const [leaveX, setLeaveX] = useState(0);
+  const onDragEnd = (_e: any, info: PanInfo) => {
+    if (info.offset.x > 200) {
+      setLeaveX(1000);
+      removeCard(card, "like");
+    }
+    if (info.offset.x < -200) {
+      setLeaveX(-1000);
+      removeCard(card, "nope");
+    }
+  };
+  return (
+    <>
+      {active ? (
+        <motion.div
+          style={{
+            background: "linear-gradient(180deg, #050505 0%, #111111 100%)",
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          onDragEnd={onDragEnd}
+          initial={{
+            scale: 1,
+          }}
+          animate={{
+            scale: 1.05,
+          }}
+          exit={{
+            x: leaveX,
+            opacity: 0,
+            scale: 0.5,
+            transition: { duration: 0.2 },
+          }}
+          className="absolute h-[430px] w-[300px] border border-gray-500 shadow-xl rounded-2xl flex flex-col justify-center items-center cursor-grab"
+        >
+          <span className="text-[140px]">{card.emoji}</span>
+          <span style={{ color: card.color }} className="text-5xl font-bold">
+            {card.name}
+          </span>
+        </motion.div>
+      ) : (
         <div
           style={{
             background: "linear-gradient(180deg, #050505 0%, #111111 100%)",
           }}
-          key={card.name}
-          className="absolute h-[430px] w-[300px] border border-gray-500 rounded-2xl flex flex-col justify-center items-center cursor-grab"
+          className="absolute h-[430px] w-[300px] rounded-2xl flex flex-col justify-center items-center cursor-grab"
         >
           <span className="text-[140px]">{card.emoji}</span>
           <span style={{ color: card.color }} className="text-5xl font-bold">
             {card.name}
           </span>
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
